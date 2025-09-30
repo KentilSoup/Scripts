878aede5886795e41aa54b8001865217bcc9c87f
Kent Fulton <kentfulton01@gmail.com>
2025-09-30 16:26:44 -0500

    Update List-Installed-Windows-Updates-v1.ps1

--- PowerShell Scripts/List-Installed-Windows-Updates-v1.ps1
+++ PowerShell Scripts/List-Installed-Windows-Updates-v1.ps1
@@ -16,7 +16,7 @@
 #       - Troubleshooting recent system changes
 #       - Auditing update history
 # ================================================
-# DISCLAIMER
+# DISCLAIMER: See LICENSE and DISCLAIMER.md in the root of this repository.
 # ================================================
 # This script is provided "as-is" without any warranties, guarantees,
 # or assurances of any kind. Use of this script is at your own risk.
